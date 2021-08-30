2cea843b9d37d6045ff8507618e02452a39a979e
ericmusa <ericmusa@umich.edu>
2021-08-30 00:05:09 -0400

    misnamed reference to app.js

--- portfolio/frontend/src/index.js
+++ portfolio/frontend/src/index.js
@@ -1,4 +1,4 @@
-import App from "./components/App";
+import App from "./components/app";
 // import "../static/styling/home.css";
 // import "../static/styling/index.scss";
 // import "../static/js/home.js";
